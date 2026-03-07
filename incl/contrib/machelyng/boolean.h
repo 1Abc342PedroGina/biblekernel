e815826e0b029d9599c1363884006edb6d01a3a2
1Abc342PedroGina <rmagalhaesjuridico5@gmail.com>
2026-03-07 09:19:15 -0300

    machelyng-kern-1990

---
+++ incl/contrib/machelyng/boolean.h
@@ -0,0 +1,24 @@
+/* 
+ * SPDX License Indentifier:MIT License
+ * Machelyng Operating System
+ * Copyright (c) 1991,1990,1989 Carnegie Mellon University
+ * All Rights Reserved.
+ * 
+ */
+
+
+
+#ifndef	_MACHELYNG_BOOLEAN_H_
+#define _MACHELYNG_BOOLEAN_H_
+
+typedef int		boolean_t;
+
+#ifndef	TRUE
+#define TRUE 1
+#endif	/* TRUE */
+
+#ifndef	FALSE
+#define FALSE	 0
+#endif	/* FALSE */
+
+#endif /* _MACHELYNG_BOOLEAN_H _ */
