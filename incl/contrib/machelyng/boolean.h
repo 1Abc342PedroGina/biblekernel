85eced3b3c17f53702a11b7637b2392f4f98c897
1Abc342PedroGina <rmagalhaesjuridico5@gmail.com>
2026-03-07 09:19:41 -0300

    machelyng-kern-1990

--- incl/contrib/machelyng/boolean.h
+++ incl/contrib/machelyng/boolean.h
@@ -1,7 +1,7 @@
 /* 
  * SPDX License Indentifier:MIT License
  * Machelyng Operating System
- * Copyright (c) 1991,1990,1989 Carnegie Mellon University
+ * Copyright (c) 1991,1990,1989 University
  * All Rights Reserved.
  * 
  */
